b12e443dd9a7257f7014861797dd0c53a95f2ae5
Dreamy <pasterakt@gmail.com>
2025-11-08 18:06:46 +0100

    Kidosy

--- Assets/Scripts/Kebab/KebabController.cs
+++ Assets/Scripts/Kebab/KebabController.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class KebabController : MonoBehaviour
 {
     [SerializeField] private GameObject KebabPrefab;
+    [SerializeField] private GameObject UnkebabPrefab;
 
 
     public void Kebabownia()
     {
+        UnkebabPrefab.SetActive(false);
         KebabPrefab.SetActive(true);
-        gameObject.SetActive(false);
     }
 
 }
